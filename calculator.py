6b782a62c047aa46b9bb122f195dcaea7e4ce610
qlovy <qlovy@sysnet.ch>
2023-11-26 14:35:10 +0100

    fonction clique

--- calculator.py
+++ calculator.py
@@ -1,20 +1,24 @@
 # importe le module tkinter
 from tkinter import *
 
+def click():
+    label.configure(text='ça fait plus')
+
+
 # la variable window appelle l'interface
 window = Tk()
 
 window.geometry("300x400")
 
-# label, définit la fenêtre
-label = Label(window, text="The Calculator")
+# label, applique du texte dans la fenêtre
+label = Label(window, text="The Calculator", fg='blue', bg='white')
 
-# place le "label"
+# place le "label" en haut de base
 label.pack()
 
 # les boutons de calculette
-button = Button(window, text = "+")
-button.pack()
+button = Button(window, text = "+", fg='white', bg='blue', command=click)
+button.pack(side='right')
 
 
 # affiche la fenêtre
